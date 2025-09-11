737eddc1dcfff5df7b3e19ec73be7380e4924000
Rozjog <muratilya@mail.ru>
2025-09-11 23:58:40 +0300

    Добавление стилей

--- src/script.js
+++ src/script.js
@@ -1,7 +1,11 @@
 const form = document.getElementById('contactForm');
 const emailInput = document.getElementById('email');
+const nameInput = document.getElementById('username');
+const phoneInput = document.getElementById('phone');
 
 emailInput.addEventListener('input', validateEmail);
+nameInput.addEventListener('input', validateName);
+phoneInput.addEventListener('input', validatePhone);
 
 function validateEmail() {
     const emailRegex = /^[A-Za-z0-9_\-\.]{2,15}@[a-z0-9\.\-_]{1,10}\.[a-z]{2,3}$/;
@@ -11,6 +15,30 @@ function validateEmail() {
     }
     else{
         showError(emailInput, "Email не соответствует формату. Пример: ivanov@mail.ru");
+        return false;
+    }
+}
+
+function validateName() {
+    const nameRegex =/^(?:[А-ЯЁ][а-яё]+(?:-[А-ЯЁ][а-яё]+){0,2}(?:\s+[А-ЯЁ][а-яё]+(?:-[А-ЯЁ][а-яё]+){0,2}){1,2})$/; 
+    if(nameRegex.test(nameInput.value)){
+        removeError(nameInput);  
+        return true;
+    }
+    else{
+        showError(nameInput, "ФИО не соответствует формату. Пример: Иванов Иван Иванович");
+        return false;
+    }
+}
+
+function validatePhone() {
+    const phoneRegex =/^(\+7|8)[\s\-]?\(?\d{3}\)?[\s\-]?\d{3}[\s\-]?\d{2}[\s\-]?\d{2}$/; 
+    if(phoneRegex.test(phoneInput.value)){
+        removeError(phoneInput);  
+        return true;
+    }
+    else{
+        showError(phoneInput, "Номер телефона не соответствует формату. Пример: +7(123) 456 78 90");
         return false;
     }
 }
@@ -28,6 +56,7 @@ function showError(input, message) {
     
     input.style.borderColor = 'red';
 }
+
 
 function removeError(input) {
     const formControl = input.parentElement;
